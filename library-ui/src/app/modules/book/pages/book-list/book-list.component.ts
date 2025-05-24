05ccb4719245bdb413f3db9770e7aaab79c5884d
Enkel007 <eshehdula22@epoka.edu.al>
2025-05-24 14:09:47 +0200

    implemented the pagination for the books list

--- library-ui/src/app/modules/book/pages/book-list/book-list.component.ts
+++ library-ui/src/app/modules/book/pages/book-list/book-list.component.ts
@@ -11,7 +11,8 @@ import { PageResponseBookResponse } from '../../../../services/models';
 export class BookListComponent implements OnInit {
   bookResponse: PageResponseBookResponse = {};
   page = 0;
-  size = 5;
+  size = 4;
+  
 
   constructor(
     private bookService: BookService,
@@ -35,5 +36,34 @@ export class BookListComponent implements OnInit {
         // Handle error appropriately, e.g., show a notification or redirect
       }
     });
+  }
+
+  goToFirstPage(){
+    this.page = 0;
+    this.findAllBooks();
+  }
+
+  goToPreviousPage() {
+    this.page--;
+    this.findAllBooks();
+  }
+
+  goToPage(page: number) {
+    this.page = page;
+    this.findAllBooks();
+  }
+
+  goToNextPage() {
+    this.page++;
+    this.findAllBooks();
+  }
+
+  goToLastPage() {
+    this.page = this.bookResponse.totalPages as number - 1;
+    this.findAllBooks();
+  }
+
+  get isLastPage(): boolean {
+    return this.page == this.bookResponse.totalPages as number - 1;
   }
 }
