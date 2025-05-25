83951d3575313acc479d8715b8bb9a19a6636772
Enkel007 <eshehdula22@epoka.edu.al>
2025-05-25 14:02:48 +0200

    implemented authguard and logout

--- library-ui/src/app/modules/book/book-routing.module.ts
+++ library-ui/src/app/modules/book/book-routing.module.ts
@@ -6,35 +6,43 @@ import { FavoritesComponent } from './pages/favorites/favorites.component';
 import { ManageBookComponent } from './pages/manage-book/manage-book.component';
 import { BorrowedBookListComponent } from './pages/borrowed-book-list/borrowed-book-list.component';
 import { ReturnBooksComponent } from './pages/return-books/return-books.component';
+import { authGuard } from '../../services/guard/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: MainComponent,
+    canActivate: [authGuard],
     children: [
       {
         path: '',
-        component: BookListComponent
+        component: BookListComponent,
+        canActivate: [authGuard]
       },
       {
         path: 'favourites',
-        component: FavoritesComponent
+        component: FavoritesComponent,
+        canActivate: [authGuard]
       },
       {
         path: 'manage',
-        component: ManageBookComponent
+        component: ManageBookComponent,
+        canActivate: [authGuard]
       },
       {
         path: 'manage/:bookId',
-        component: ManageBookComponent
+        component: ManageBookComponent,
+        canActivate: [authGuard]
       },
       {
         path: 'my-borrowed-books',
-        component: BorrowedBookListComponent
+        component: BorrowedBookListComponent,
+        canActivate: [authGuard]
       },
       {
         path: 'returned-books',
-        component: ReturnBooksComponent
+        component: ReturnBooksComponent,
+        canActivate: [authGuard]
       }
     ]
   }
--- library-ui/src/app/services/token/token.service.ts
+++ library-ui/src/app/services/token/token.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TokenService {
+  isTokenNotValid() {
+    return !this.isTokenValid();
+  }
+
+  private isTokenValid() {
+    const token = this.token;
+    if(!token){
+      return false;
+    }
+    //decode the token
+    const jwtHelper = new JwtHelperService();
+    // check expiration date
+    const isTokenExpired = jwtHelper.isTokenExpired(token);
+    if(isTokenExpired) {
+      localStorage.clear();
+      return false;
+    }
+    return true;
+  }
 
   set token(token: string){
     localStorage.setItem('token', token);
